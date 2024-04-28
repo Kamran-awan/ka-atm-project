a6923a568989b2ad115766dca532343dd00e2862
Kamran-awan <kamranawan343@gmail.com>
2024-04-28 16:26:57 +0500

    first commit

---
+++ README.md
@@ -0,0 +1,1 @@
+"# ka-atm-project" 
---
+++ index.js
@@ -0,0 +1,39 @@
+#! /usr/bin/env node
+import inquirer from "inquirer";
+let myBalnace = 10000;
+let myPin = 2345;
+let pinAnswer = await inquirer.prompt([
+    {
+        name: "pin",
+        message: "Enter your Pin Code",
+        type: "number",
+    }
+]);
+if (pinAnswer.pin === myPin) {
+    console.log("correct password");
+    let operationAns = await inquirer.prompt([
+        {
+            name: "operation",
+            message: "select option",
+            type: "list",
+            choices: ["withdraw", "check balance", "transfer amount",]
+        }
+    ]);
+    if (operationAns.operation === "withdraw") {
+        let amountAns = await inquirer.prompt([
+            {
+                name: "amount",
+                message: "entre your amount",
+                type: "number",
+            }
+        ]);
+        myBalnace -= amountAns.amount;
+        console.log("your remaining balance is" + myBalnace);
+    }
+    else if (operationAns.operation === "check balance") {
+        console.log(`your remaining balance is ${myBalnace}`); //Template literal
+    }
+}
+else {
+    console.log("incorrect pin code");
+}
